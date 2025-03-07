abfa92b1007ad08a973914dc0b08003de7bf0e41
atlaspolat <apolat21@ku.edu.tr>
2025-03-07 15:03:20 +0300

    Matrix Interface

---
+++ src/matrix/Matrix.java
@@ -0,0 +1,18 @@
+package matrix;
+
+public interface Matrix {
+    int getRows();
+    int getCols();
+    
+    double get(int row, int col);
+    void set(int row, int col, double value);
+    
+    Matrix add(Matrix other);
+    Matrix multiply(Matrix other);
+    Matrix transpose();
+    
+    Matrix scalarMultiply(double scalar);
+    
+    boolean isSquare();
+    double determinant();  // Only for square matrices
+}
